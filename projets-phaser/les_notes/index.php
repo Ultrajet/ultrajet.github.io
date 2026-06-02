77d47446c826ac3fcc8330978d5ae993885e9052
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2026-06-02 19:42:51 +0200

    réorganisation

---
+++ projets-phaser/les_notes/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projet-jeu-notes</title>
+</head>
+<style>
+    body {
+        background: black;
+        color: white;
+        font-family: arial;
+    }
+    .container {
+        width: 600px;
+        border: 1px solid white;
+        margin: 0 auto;
+        padding: 0 12px;
+    }
+    a {
+        color: yellow;
+    }
+</style>
+<body>
+    <div class="container">
+        <h1>projet-jeu-notes</h1>
+        <hr>
+        <p>Du texte du texte du texte</p>
+        <p><a href="https://www.php.net/" target="_blank">Un lien plein de secret....</a></p>
+    </div>
+</body>
+</html>
